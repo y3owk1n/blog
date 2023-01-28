08999225a742f63632fac3ec6954a3b5878d3cc5
y3owk1n <wongyeowkin@gmail.com>
2023-01-28 14:24:32 +0800

    h2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { H1 } from "@/components/typography/H1";
 import { Container } from "@/components/Container";
 import React from "react";
+import { H2 } from "@/components/typography/H2";
 
 const Page = () => {
     return (
         <main>
             <Container>
                 <H1>The Joke Tax Chronicles</H1>
+                <H2>The Kings Plan</H2>
             </Container>
         </main>
     );
---
+++ src/components/typography/H2.tsx
@@ -0,0 +1,13 @@
+import type { ReactNode } from "react";
+
+interface Props {
+    children: ReactNode;
+}
+
+export function H2({ children }: Props) {
+    return (
+        <h2 className="mt-10 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0 dark:border-b-slate-700">
+            {children}
+        </h2>
+    );
+}
