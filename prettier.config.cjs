ed055ec9969461370ec4e5b744ac29101c2c79a9
y3owk1n <wongyeowkin@gmail.com>
2023-01-28 13:31:13 +0800

    init

---
+++ prettier.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: true,
+    singleQuote: false,
+    bracketSpacing: true,
+    bracketSameLine: true,
+    jsxSingleQuote: false,
+    printWidth: 80,
+    arrowParens: "always",
+    singleAttributePerLine: true,
+  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+};
