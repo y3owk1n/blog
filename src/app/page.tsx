2684fb264c8e8aecb47533414c5655d2f77f1037
y3owk1n <wongyeowkin@gmail.com>
2023-01-28 14:25:33 +0800

    h3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { H1 } from "@/components/typography/H1";
 import { Container } from "@/components/Container";
 import React from "react";
 import { H2 } from "@/components/typography/H2";
+import { H3 } from "@/components/typography/H3";
 
 const Page = () => {
     return (
@@ -9,6 +10,7 @@ const Page = () => {
             <Container>
                 <H1>The Joke Tax Chronicles</H1>
                 <H2>The Kings Plan</H2>
+                <H3>The Joke Tax</H3>
             </Container>
         </main>
     );
---
+++ src/components/typography/H3.tsx
@@ -0,0 +1,13 @@
+import type { ReactNode } from "react";
+
+interface Props {
+    children: ReactNode;
+}
+
+export function H3({ children }: Props) {
+    return (
+        <h3 className="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">
+            {children}
+        </h3>
+    );
+}
