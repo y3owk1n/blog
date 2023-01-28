ba8753a0cd0d6b1ac5f111e0b21b2fe88f734b76
y3owk1n <wongyeowkin@gmail.com>
2023-01-28 15:42:05 +0800

    dialog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,17 @@ import {
     AccordionItem,
     AccordionTrigger,
 } from "@/components/Accordion";
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/AlertDialog";
 
 const tableContents: TableType = {
     title: ["King's Treasury", "People's happiness"],
@@ -92,6 +103,25 @@ const Page = () => {
                         </AccordionContent>
                     </AccordionItem>
                 </Accordion>
+                <AlertDialog>
+                    <AlertDialogTrigger>Open</AlertDialogTrigger>
+                    <AlertDialogContent>
+                        <AlertDialogHeader>
+                            <AlertDialogTitle>
+                                Are you sure absolutely sure?
+                            </AlertDialogTitle>
+                            <AlertDialogDescription>
+                                This action cannot be undone. This will
+                                permanently delete your account and remove your
+                                data from our servers.
+                            </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                            <AlertDialogCancel>Cancel</AlertDialogCancel>
+                            <AlertDialogAction>Continue</AlertDialogAction>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialog>
             </Container>
         </main>
     );
