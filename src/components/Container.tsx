145b83692fd2b15ccf3424f5f183afcb18fc6541
y3owk1n <wongyeowkin@gmail.com>
2023-01-28 14:56:21 +0800

    addad y padding

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const Container = ({ children }: Props) => {
     return (
-        <div className="mx-auto max-w-2xl px-4 pb-40 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="mx-auto max-w-2xl px-4 py-20 sm:px-6 lg:max-w-7xl lg:px-8">
             {children}
         </div>
     );
