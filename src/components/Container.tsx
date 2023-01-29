e6940c116fe382162c14e88a01d6ee038f0e9b57
y3owk1n <wongyeowkin@gmail.com>
2023-01-29 11:33:37 +0800

    provide classnames for container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,13 +1,19 @@
+import { classNames } from "@/lib/classNames";
 import type { ReactNode } from "react";
 import React from "react";
 
 interface Props {
     children: ReactNode;
+    className?: string;
 }
 
-const Container = ({ children }: Props) => {
+const Container = ({ children, className = "" }: Props) => {
     return (
-        <div className="mx-auto max-w-2xl px-4 py-20 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div
+            className={classNames(
+                "mx-auto max-w-2xl px-4 py-20 sm:px-6 lg:max-w-7xl lg:px-8",
+                className
+            )}>
             {children}
         </div>
     );
