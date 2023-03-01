0d9b6eb8372340b76b694952cca41e8551037f92
y3owk1n <wongyeowkin@gmail.com>
2023-03-01 21:10:01 +0800

    fixed ui import for toast

--- src/lib/hooks/useToast.ts
+++ src/lib/hooks/useToast.ts
@@ -1,5 +1,5 @@
 // Inspired by react-hot-toast library
-import type { ToastActionElement, ToastProps } from "@/components/Toast";
+import type { ToastActionElement, ToastProps } from "@/components/ui/Toast";
 import * as React from "react";
 
 const TOAST_LIMIT = 1;
