c870f96be760b24075a3246af9efe502555ed20b
y3owk1n <wongyeowkin@gmail.com>
2023-03-05 11:42:15 +0800

    fixed sidebar

--- src/app/ui/layout.tsx
+++ src/app/ui/layout.tsx
@@ -9,7 +9,7 @@ interface UiLayoutProps {
 export default function UiLayout({ children }: UiLayoutProps) {
     return (
         <div className="flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
-            <aside className="fixed top-14 z-30 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 overflow-y-auto border-r border-r-slate-100 md:sticky md:block">
+            <aside className="fixed top-20 z-30 hidden h-[calc(100vh-10vh)] w-full shrink-0 overflow-y-auto border-r border-r-slate-100 md:sticky md:block">
                 <ScrollArea className="pr-4">
                     <SidebarNav items={uiConfig.sidebarNav} />
                 </ScrollArea>
