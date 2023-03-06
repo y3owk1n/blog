3ceaa108baeb31bf4903bb9da8384cd90f97c32e
y3owk1n <wongyeowkin@gmail.com>
2023-03-07 00:45:18 +0800

    refactor aspectratio demo

--- src/components/examples/AspectRatioDemo.tsx
+++ src/components/examples/AspectRatioDemo.tsx
@@ -1,21 +1,20 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
-
-import { AspectRatio } from "@/components/ui/AspectRatio";
+import CImage from "@/components/ui/Image";
 
 const AspectRatioDemo = () => {
     return (
         <div className="w-[450px]">
-            <AspectRatio ratio={16 / 9}>
-                <Image
-                    src="https://images.unsplash.com/photo-1576075796033-848c2a5f3696?w=800&dpr=2&q=80"
-                    alt="Photo by Alvaro Pinot"
-                    fill
-                    className="rounded-md object-cover"
-                />
-            </AspectRatio>
+            <CImage
+                aspectRatioProps={{
+                    ratio: 16 / 9,
+                }}
+                imageProps={{
+                    src: "https://images.unsplash.com/photo-1576075796033-848c2a5f3696?w=800&dpr=2&q=80",
+                    alt: "Photo by Alvaro Pinot",
+                }}
+            />
         </div>
     );
 };
