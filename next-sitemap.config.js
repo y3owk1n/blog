9706fbc2ff6c876afb61b68bb07f41ad8947dd83
y3owk1n <wongyeowkin@gmail.com>
2023-03-09 20:55:40 +0800

    next sitemap init

---
+++ next-sitemap.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: process.env.SITE_URL || "https://www.kylewong.my",
+    generateRobotsTxt: true,
+    robotsTxtOptions: {
+        policies: [
+            {
+                userAgent: "*",
+                allow: "/",
+            },
+        ],
+    },
+    sitemapSize: 1000,
+};
+
+export default config;
