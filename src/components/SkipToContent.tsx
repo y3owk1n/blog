a753cab8fa769b75510ca33e5b9a9ea6a2ccc34d
Kyle Wong <wongyeowkin@gmail.com>
2023-07-22 17:41:49 +0800

    chore: make skip to content button looks nicer

--- src/components/SkipToContent.tsx
+++ src/components/SkipToContent.tsx
@@ -5,7 +5,7 @@ const SkipToContent = () => {
     return (
         <Link
             href="/#content"
-            className="absolute left-0 top-0 block -translate-x-full rounded bg-gradient-to-br from-teal-400 via-blue-500 to-purple-600 px-4 py-3 text-sm font-bold uppercase tracking-widest text-white focus-visible:translate-x-0">
+            className="absolute left-0 top-0 block -translate-x-full rounded bg-foreground px-4 py-3 text-sm font-bold uppercase text-background  focus-visible:translate-x-0">
             Skip to Content
         </Link>
     );
