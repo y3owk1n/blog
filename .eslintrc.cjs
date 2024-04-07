1331481d819b9d2d4ac802525c1a93736829423e
y3owk1n <wongyeowkin@gmail.com>
2024-04-07 15:57:39 +0800

    build: Use vercel style guide for eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,41 +1,36 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
 	root: true,
+	plugins: ["only-warn", "@typescript-eslint"],
+	extends: [
+		"@vercel/style-guide/eslint/node",
+		"@vercel/style-guide/eslint/typescript",
+		"@vercel/style-guide/eslint/browser",
+		"@vercel/style-guide/eslint/react",
+		"@vercel/style-guide/eslint/next",
+	].map(require.resolve),
+	globals: {
+		React: true,
+		JSX: true,
+	},
+	ignorePatterns: [
+		// Ignore dotfiles
+		"**/*.js",
+		"**/*.mjs",
+		"**/*.cjs",
+		"**/*.config.cjs",
+		"node_modules/",
+		"dist/",
+	],
 	parser: "@typescript-eslint/parser",
 	parserOptions: {
 		project: true,
 		extraFileExtensions: [".json"],
 	},
-	plugins: ["@typescript-eslint"],
-	extends: [
-		"next/core-web-vitals",
-		"plugin:@typescript-eslint/recommended-type-checked",
-		"plugin:@typescript-eslint/stylistic-type-checked",
-	],
 	rules: {
-		// These opinionated rules are enabled in stylistic-type-checked above.
-		// Feel free to reconfigure them to your own preference.
-		"@typescript-eslint/array-type": "off",
-		"@typescript-eslint/consistent-type-definitions": "off",
-
-		"@typescript-eslint/consistent-type-imports": [
-			"warn",
-			{
-				prefer: "type-imports",
-				fixStyle: "inline-type-imports",
-			},
-		],
-		"@typescript-eslint/no-unused-vars": [
-			"warn",
-			{ argsIgnorePattern: "^_" },
-		],
-		"@typescript-eslint/require-await": "off",
-		"@typescript-eslint/no-misused-promises": [
-			"error",
-			{
-				checksVoidReturn: { attributes: false },
-			},
-		],
+		"import/no-default-export": "off",
+		"import/order": "off",
+		"@typescript-eslint/restrict-template-expressions": "off",
 	},
 };
 
