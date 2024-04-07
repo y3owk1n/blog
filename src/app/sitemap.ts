052912b7f5be33b8eda5b5abfef4202892645251
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-04-07 18:39:23 +0800

    fix: use native sitemap and seo related functionality (#87)

---
+++ src/app/sitemap.ts
@@ -0,0 +1,25 @@
+import { type MetadataRoute } from "next";
+import config from "../../keystatic.config";
+
+import { createReader } from "@keystatic/core/reader";
+
+const reader = createReader(process.cwd(), config);
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+	const dateNow = new Date();
+
+	const postsData = await reader.collections.posts.all();
+
+	const posts = postsData.map((post) => ({
+		url: `https://www.kylewong.my/posts/${post.slug}`,
+		lastModified: post.entry.date ? new Date(post.entry.date) : new Date(),
+	}));
+
+	return [
+		{
+			url: "https://www.kylewong.my",
+			lastModified: dateNow,
+		},
+		...posts,
+	];
+}
